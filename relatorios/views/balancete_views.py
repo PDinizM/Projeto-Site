558e208064d6e16c262f6c75f70c7c81daa6881e
Pedro Diniz <phdm2002@gmail.com>
2025-06-16 13:11:24 -0300

    Co-authored-by: granerc <granerc@users.noreply.github.com>

--- relatorios/views/balancete_views.py
+++ relatorios/views/balancete_views.py
@@ -4,60 +4,47 @@ from relatorios.utils.balancete_utils import relatorioBalanceteDominio, relatori
 from relatorios.utils.conexao import conectar_dominio
 from relatorios.utils.competencias import formata_data
 
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]  # Primeiro IP da lista
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
-
-
 def pesquisa(request):
+    if request.method == 'GET':
+        return render(request, 'relatorios/balancete/balancete_form.html')
+    
+    # TRATATIVAS DO POST\
+    
+    tipo_balancete = request.POST.get('balancete_tipo')
+    empresa = request.POST.get('empresa')
+    banco = request.POST.get('conexao')
+    data_inicial = request.POST.get('dataInicial')
+    data_final = request.POST.get('dataFinal')
+    transferencia = 'S' if request.POST.get('transferencia') else 'N'
+    zeramento = 'S' if request.POST.get('zeramento') else 'N'
 
-    ip = get_client_ip(request)
-    print(f"IP do usuário: {ip}")
+    conexao = conectar_dominio(banco)
+    
+    if tipo_balancete == 'normal':
+        relatorio = relatorioBalanceteDominio(
+            empresa, data_inicial, data_final,
+            zeramento, transferencia, conexao
+    )
+    elif tipo_balancete == 'plano_referencial':
+        relatorio = relatorioBalanceteECF(
+            empresa, data_inicial, data_final,
+            zeramento, transferencia, conexao
+    )
 
-    if request.method == 'POST':
-        tipo_balancete = request.POST.get('balancete_tipo')
-        empresa = request.POST.get('empresa')
-        banco = request.POST.get('conexao')
-        data_inicial = request.POST.get('dataInicial')
-        data_final = request.POST.get('dataFinal')
-        transferencia = 'S' if request.POST.get('transferencia') else 'N'
-        zeramento = 'S' if request.POST.get('zeramento') else 'N'
+    relatorio = relatorio.rename(columns={
+        "Saldo Anterior": "saldo_anterior",
+        "debitoAtual": "debito_atual",
+        "creditoAtual": "credito_atual",
+        "Saldo Atual": "saldo_atual"
+    })
 
-        conexao = conectar_dominio(banco)
-        
-        if tipo_balancete == 'normal':
-            relatorio = relatorioBalanceteDominio(
-                empresa, data_inicial, data_final,
-                zeramento, transferencia, conexao
-        )
-        elif tipo_balancete == 'plano_referencial':
-            relatorio = relatorioBalanceteECF(
-                empresa, data_inicial, data_final,
-                zeramento, transferencia, conexao
-        )
+    relatorio_json = relatorio.to_dict(orient='records')
 
-        relatorio = relatorio.rename(columns={
-            "Saldo Anterior": "saldo_anterior",
-            "debitoAtual": "debito_atual",
-            "creditoAtual": "credito_atual",
-            "Saldo Atual": "saldo_atual"
-        })
-        
-        relatorio.to_excel(r'C:\Users\pedrodiniz\Desktop\A\teste.xlsx', index=False)
+    request.session['relatorio_json'] = relatorio_json
+    request.session['data_inicial'] = data_inicial
+    request.session['data_final'] = data_final
 
-        relatorio_json = relatorio.to_dict(orient='records')
-
-        request.session['relatorio_json'] = relatorio_json
-        request.session['data_inicial'] = data_inicial
-        request.session['data_final'] = data_final
-
-        return redirect('relatorios:resultado')
-
-    return render(request, 'relatorios/balancete/balancete_form.html')
+    return redirect('relatorios:resultado')
 
 
 def resultado(request):
